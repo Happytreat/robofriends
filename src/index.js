f7f21378edbf468e5bb2090d426c20d28e7c135f
Happytreat <melodiessim98@gmail.com>
2019-04-27 23:28:30 +0800

    Add createStore for reducer and provider to pass store down to each components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { searchRobots } from './reducers';
 
-ReactDOM.render(<App />
+const store = createStore(searchRobots); 
+
+ReactDOM.render(
+				<Provider store={store}>
+					<App />
+				</Provider>
 				, document.getElementById('root'));
 serviceWorker.unregister();
